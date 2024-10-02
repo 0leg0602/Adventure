5116b0f10d44799025f7e69cd5924f19ddc7aad7
0leg0602 <olegpolyakov0602@gmail.com>
2024-10-02 00:39:07 -0400

    Updated chapter 1 Started creating combat mechanics

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -6,6 +6,7 @@ package project;
         End Time:
 */
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -26,16 +27,80 @@ public class Main {
     public static class Player {
         int health = 100;
         Inventory inventory = new Inventory();
+
+        void take_damage(int damage) {
+            println("You took " + damage + " points of damage.");
+            println("Your health is at " + health + " - " + damage + " = " + (health - damage) + ".");
+            health -= damage;
+        }
+
+        public void attack(Enemy enemy, int damage, String move) {
+            switch (move) {
+                case "repel" -> {
+                    println(enemy.name + " anticipated the attack from that direction and easily repelled it.");
+                    take_damage(enemy.attack_damage);
+                }
+                case "counter" -> {
+                    println("You attack and land a successful hit, dealing damage.");
+                    enemy.take_damage(damage);
+                    println("However, " + enemy.name + " quickly retaliates with a counterattack.");
+                    take_damage(enemy.attack_damage);
+                }
+                case "block" -> {
+                    println(enemy.name + " The monster attempted to block your attack.");
+                    if (enemy.block < damage) {
+                        println("However, it was only partially successful, allowing you to deal some damage anyway.");
+                        println(enemy.block + " damage blocked.");
+                        enemy.take_damage(damage - enemy.block);
+                    } else {
+                        println(enemy.name + " fully blocked your attack, as you didn't deal enough damage to break through its defenses.");
+                    }
+                }
+                case "none" -> {
+                    println("You attack and land a successful hit, dealing damage.");
+                    enemy.take_damage(damage);
+                }
+            }
+
+        }
+    }
+
+    static Player player;
+
+    public static class Enemy {
+        String name;
+        int health;
+        int block;
+        int attack_damage;
+        int level;
+        int frustration;
+        int frustration_limit;
+        String[] moves_str;
+        String[] moves;
+        String escape_death;
+
+
+        public void take_damage(int damage) {
+            println(name + " took " + damage + " points of damage.");
+            println(name + " health is at " + health + " - " + damage + " = " + (health - damage) + ".");
+            health -= damage;
+        }
     }
 
 
     public static void print_options(String[] options) {
         for (int i = 0; i < options.length; i++) {
-            println((i + 1) + ". " + options[i]);
+            if (!options[i].equals("none")) {
+                if (options[i].equals("back")) {
+                    println("0. back");
+                } else {
+                    println((i + 1) + ". " + options[i]);
+                }
+            }
         }
     }
 
-    public static int get_user_input(int available_range) {
+    public static int get_user_input(int min, int max) {
         skip = false;
         while (true) {
             print(">>> ");
@@ -58,7 +123,7 @@ public class Main {
 
             try {
                 int int_input = Integer.parseInt(input);
-                if (int_input < 1 || int_input > available_range) {
+                if (int_input < min || int_input > max) {
                     throw new Exception("Not in range");
                 }
                 return int_input;
@@ -151,74 +216,182 @@ public class Main {
         print("╝\n");
     }
 
-    public static void draw_ascii(String name){
+    public static void fight(String enemy_str) {
+        print_banner(" THE FIGHT BEGINS ");
+
+
+        Enemy enemy = new Enemy();
+
+        switch (enemy_str) {
+            case "forester's monster" -> {
+                enemy.name = "The forester's monster";
+                enemy.health = 24;
+                enemy.block = 3;
+                enemy.attack_damage = 20;
+                enemy.level = 5;
+                enemy.frustration = 0;
+                enemy.frustration_limit = 4;
+                enemy.moves_str = new String[]{"gazes intently at you.", "leaned to the right.", "leaned to the left.", "crouched down.", "dropped his hands tiredly"};
+//                                          top bottom left right
+                enemy.moves = new String[]{"repel,none repel,none repel,none repel,none", "counter,none counter,none counter,none repel,attack", "counter,none counter,none repel,attack counter,none", "block,none block,none block,none block,none", "none,none none,none none,none none,none"};
+
+                enemy.escape_death = "You tried to escape from the forester's monster,\nand you managed to run to the front door of the house.\nUnfortunately, when you tried to open it, you found that it was locked.\nBy the time you turned around, it was already too late.";
+            }
+            case "bat" -> {
+                enemy.name = "bat";
+                enemy.health = 30;
+                enemy.level = 3;
+                enemy.moves_str = new String[]{"gazes intently at you", "leaned to the right", "leaned to the left", "crouched down"};
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + enemy_str);
+        }
+
+        Random rand = new Random();
+
+        while (enemy.health > 0) {
+            int move;
+            if (enemy.frustration < enemy.frustration_limit) {
+                move = rand.nextInt(enemy.moves_str.length - 1);
+            } else {
+                move = enemy.moves_str.length;
+            }
+            println(enemy.name + " " + enemy.moves_str[move]);
+            while (true) {
+                println("What will you do?");
+                print_options(new String[]{"attack", "block", "action", "escape"});
+                int input = get_user_input(1, 4);
+                if (input == 1) {
+
+                    StringBuilder weapon_options_string = new StringBuilder();
+                    for (String weapon : new String[]{"knife", (player.inventory.axe > 0) ? "axe" : "none", "back"}) {
+                        if (!weapon.equals("none")) {
+                            weapon_options_string.append(weapon).append(" ");
+                        }
+
+                    }
+                    String[] weapon_options = weapon_options_string.toString().split(" ");
+                    print_options(weapon_options);
+                    input = get_user_input(0, weapon_options.length - 1);
+
+                    if (input == 0) {
+                        continue;
+                    }
+
+                    int damage = switch (weapon_options[input - 1]) {
+                        case "knife" -> 5;
+                        case "axe" -> 12;
+                        default -> throw new IllegalStateException("Unexpected value: " + weapon_options[input - 1]);
+                    };
+
+                    println("How do you hit with a " + weapon_options[input - 1] + "?");
+                    print_options(new String[]{"from above", "from below", "from the left", "from the right", "back"});
+                    input = get_user_input(0, 4);
+                    if (input == 0) {
+                        continue;
+                    }
+                    player.attack(enemy, damage, enemy.moves[move].split(" ")[input - 1].split(",")[0]);
+                    break;
+
+                } else if (input == 2) {
+                    break;
+//                    TODO
+                } else if (input == 3) {
+//                    TODO
+                }
+
+                if (input == 4) {
+                    println("Are you sure you want to escape?");
+                    print_options(new String[]{"yes", "back"});
+                    input = get_user_input(0, 1);
+                    if (input == 1) {
+                        println(enemy.escape_death);
+                        gameover();
+                    } else if (input == 0) {
+                        continue;
+                    }
+                }
+            }
+
+//            enemy.attack();
+//          TODO
+        }
+
+        print_banner(" THE FIGHT IS OVER ");
+        println("You won!");
+        println(enemy.name + " is defeated!");
+
+//        TODO
+    }
+
+
+    public static void draw_ascii(String name) {
         switch (name) {
             case "gameover" -> println("""
-            ╔══════════════════════════════════════════════════════════════════════════════════╗
-            ║  _|_|_|    _|_|    _|      _|  _|_|_|_|    _|_|    _|      _|  _|_|_|_|  _|_|_|  ║\s
-            ║_|        _|    _|  _|_|  _|_|  _|        _|    _|  _|      _|  _|        _|    _|║\s
-            ║_|  _|_|  _|_|_|_|  _|  _|  _|  _|_|_|    _|    _|  _|      _|  _|_|_|    _|_|_|  ║\s
-            ║_|    _|  _|    _|  _|      _|  _|        _|    _|    _|  _|    _|        _|    _|║\s
-            ║  _|_|_|  _|    _|  _|      _|  _|_|_|_|    _|_|        _|      _|_|_|_|  _|    _|║\s
-            ╚══════════════════════════════════════════════════════════════════════════════════╝\s
-            """);
+                    ╔══════════════════════════════════════════════════════════════════════════════════╗
+                    ║  _|_|_|    _|_|    _|      _|  _|_|_|_|    _|_|    _|      _|  _|_|_|_|  _|_|_|  ║\s
+                    ║_|        _|    _|  _|_|  _|_|  _|        _|    _|  _|      _|  _|        _|    _|║\s
+                    ║_|  _|_|  _|_|_|_|  _|  _|  _|  _|_|_|    _|    _|  _|      _|  _|_|_|    _|_|_|  ║\s
+                    ║_|    _|  _|    _|  _|      _|  _|        _|    _|    _|  _|    _|        _|    _|║\s
+                    ║  _|_|_|  _|    _|  _|      _|  _|_|_|_|    _|_|        _|      _|_|_|_|  _|    _|║\s
+                    ╚══════════════════════════════════════════════════════════════════════════════════╝\s
+                    """);
             case "axe" -> println("""
-                ╔════════════╗
-                ║  /'-./\\_   ║
-                ║ :    ||,>  ║
-                ║  \\.-'||    ║
-                ║      ||    ║
-                ║      ||    ║
-                ║      ||    ║
-                ╚════════════╝
-                """);
+                    ╔════════════╗
+                    ║  /'-./\\_   ║
+                    ║ :    ||,>  ║
+                    ║  \\.-'||    ║
+                    ║      ||    ║
+                    ║      ||    ║
+                    ║      ||    ║
+                    ╚════════════╝
+                    """);
             case "lantern" -> println("""
-            ╔═════════╗
-            ║   ___   ║
-            ║  |   |  ║
-            ║   \\_/   ║
-            ║    :    ║
-            ║    o    ║
-            ║ .d8|8b. ║
-            ║ T""T""T ║
-            ║ ] (|) [ ║
-            ║  [_|_]  ║
-            ╚═════════╝
-            """);
+                    ╔═════════╗
+                    ║   ___   ║
+                    ║  |   |  ║
+                    ║   \\_/   ║
+                    ║    :    ║
+                    ║    o    ║
+                    ║ .d8|8b. ║
+                    ║ T""T""T ║
+                    ║ ] (|) [ ║
+                    ║  [_|_]  ║
+                    ╚═════════╝
+                    """);
             case "first aid kit" -> println("""
-                ╔══════════════════════╗
-                ║      _____________   ║
-                ║     /     __      /| ║
-                ║    /   __/ /_    / / ║
-                ║   /   /_  __/   / // ║
-                ║  /     /_/     / //  ║
-                ║ /_____________/ //   ║
-                ║ |______&______|//    ║
-                ║ |_____________|/     ║
-                ╚══════════════════════╝
-                """);
+                    ╔══════════════════════╗
+                    ║      _____________   ║
+                    ║     /     __      /| ║
+                    ║    /   __/ /_    / / ║
+                    ║   /   /_  __/   / // ║
+                    ║  /     /_/     / //  ║
+                    ║ /_____________/ //   ║
+                    ║ |______&______|//    ║
+                    ║ |_____________|/     ║
+                    ╚══════════════════════╝
+                    """);
             case "rope" -> println("""
-                ╔════════╗
-                ║  \\\\//  ║
-                ║   //   ║
-                ║  //\\\\  ║
-                ║  \\\\//  ║
-                ║   //   ║
-                ║  //\\\\  ║
-                ║  \\\\//  ║
-                ║   //   ║
-                ║  //\\\\  ║
-                ║  \\\\//  ║
-                ║   //   ║
-                ║  //\\\\  ║
-                ║  \\\\//  ║
-                ║   //   ║
-                ║  //\\\\  ║
-                ║  \\\\//  ║
-                ║   //   ║
-                ║  //\\\\  ║
-                ╚════════╝
-                """);
+                    ╔════════╗
+                    ║  \\\\//  ║
+                    ║   //   ║
+                    ║  //\\\\  ║
+                    ║  \\\\//  ║
+                    ║   //   ║
+                    ║  //\\\\  ║
+                    ║  \\\\//  ║
+                    ║   //   ║
+                    ║  //\\\\  ║
+                    ║  \\\\//  ║
+                    ║   //   ║
+                    ║  //\\\\  ║
+                    ║  \\\\//  ║
+                    ║   //   ║
+                    ║  //\\\\  ║
+                    ║  \\\\//  ║
+                    ║   //   ║
+                    ║  //\\\\  ║
+                    ╚════════╝
+                    """);
         }
     }
 
@@ -232,7 +405,7 @@ public class Main {
 
     public static void start_game() {
 
-        Player player = new Player();
+        player = new Player();
 
 
         println("You decided to go hunting what will you take with you: ");
@@ -241,7 +414,7 @@ public class Main {
 
         print_options(tools_options);
 
-        int input = get_user_input(4);
+        int input = get_user_input(1, 4);
 
         switch (input) {
             case 1 -> {
@@ -273,7 +446,7 @@ public class Main {
         println("Ahead, you spotted a hut - an abandoned, overgrown forester's cottage.");
 
         print_options(new String[]{"Go into the forester's house", "Walk past the forester's house"});
-        input = get_user_input(2);
+        input = get_user_input(1, 2);
 
         if (input == 1) {
             chapter1();
@@ -286,7 +459,7 @@ public class Main {
                 println("Under its faint light, you continued to wander through the forest in search of shelter.");
                 println("After walking a little farther, you came across the forester's house once again.");
                 print_options(new String[]{"Go into the forester's house", "Walk past the forester's house"});
-                input = get_user_input(2);
+                input = get_user_input(1, 2);
                 if (input == 1) {
                     chapter1();
                 } else if (input == 2) {
@@ -302,7 +475,7 @@ public class Main {
                 println("the forest's density blocked out even a glimpse of moonlight.");
                 println("What will you do next?");
                 print_options(new String[]{"Lay down on the ground, trying to rest until the morning", "Attempt to make your way blindly through the forest in search of a source of light"});
-                input = get_user_input(2);
+                input = get_user_input(1, 2);
                 if (input == 1) {
                     println("You lay down on the ground and fell asleep.");
                     println("But unfortunately, you didn't wake up in the morning.");
@@ -325,11 +498,38 @@ public class Main {
         print_banner("Chapter 1 : An Abandoned, overgrown forester's cottage");
         println("You have opened the door entered the forester's cottage.");
         println("You walked into the living room.");
-        println("To be continued...");
-        gameover();
-//        println("What are you going to do next?");
-//        print_options(new String[]{"Sleep on the floor in the living room", "Explore the room to your right (kitchen)", "Explore the attic"});
-//        int input = get_user_input(3);
+        println("What are you going to do next?");
+        print_options(new String[]{"Sleep on the floor in the living room", "Explore the room to your right (kitchen)", "Explore the attic"});
+        int input = get_user_input(1, 3);
+        if (input == 1) {
+            println("You had chosen to sleep on the floor in the living room, hoping to catch some rest,");
+            println("but your attempt was disrupted by a noise coming from the room on your right.");
+            print_options(new String[]{"Ignore the noise", "Check the source of the noise."});
+            input = get_user_input(1, 2);
+            if (input == 1) {
+                println("You decided that the noise was just your imagination and you shouldn't worry.");
+//                TODO
+            } else if (input == 2) {
+                println("You approached the room to the right, being cautious as you entered.");
+                println("But the creaking of the floor woke up the one who was sleeping peacefully there.");
+                println("A forest monster has attacked you.");
+                fight("forester's monster");
+
+//                TODO
+            }
+
+        } else if (input == 2) {
+            println("You approached the room to the right, being cautious as you entered.");
+            println("But the creaking of the floor woke up the one who was sleeping peacefully there.");
+            println("A forest monster has attacked you.");
+            fight("forester's monster");
+
+//            TODO
+
+        } else if (input == 3) {
+//            TODO
+
+        }
 
     }
 
